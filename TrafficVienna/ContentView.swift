d1d4e982ca778a02dd12f3b47ec60549ed21539e
Vaniawl <idovhosheia@gmail.com>
2025-11-07 09:38:47 +0100

    Initial Commit

---
+++ TrafficVienna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrafficVienna
+//
+//  Created by Ivan Dovhosheia on 07.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrafficVienna/TrafficViennaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficViennaApp.swift
+//  TrafficVienna
+//
+//  Created by Ivan Dovhosheia on 07.11.25.
+//
+
+import SwiftUI
+
+@main
+struct TrafficViennaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
