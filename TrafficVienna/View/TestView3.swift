c531fab8b1dd31fb7a5e0656cdd78d4935ab9353
Vaniawl <idovhosheia@gmail.com>
2025-11-15 14:52:27 +0100

    added third testview screen

---
+++ TrafficVienna/View/TestView3.swift
@@ -0,0 +1,18 @@
+//
+//  TestView3.swift
+//  TrafficVienna
+//
+//  Created by Ivan Dovhosheia on 15.11.25.
+//
+
+import SwiftUI
+
+struct TestView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView3()
+}
